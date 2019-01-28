472df7d6144a87d9ac62904c6a8ef1ce73602a0f
Yuriy <genius922@yandex.ru>
2019-01-28 15:00:00 +0300

    stack

--- 05-heap/src/main/java/ru/otus/Heap.java
+++ 05-heap/src/main/java/ru/otus/Heap.java
@@ -1,11 +1,13 @@
 package ru.otus;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Heap<T extends Comparable<T>> {
 
     private T[] data;
     private int size;
+    private static Stack<Integer> stack = new Stack<>();
 
     public Heap(T[] arr) {
         if (arr == null)
@@ -20,28 +22,37 @@ public class Heap<T extends Comparable<T>> {
         Heap<T> h = new Heap<>(arr);
         for (int i = 0; i < h.size(); i++) {
             h.swap(0, h.size()-i-1);
-            h.drown(0, h.size()-i-1);
+            stack.push(0);
+            stack.push(h.size()-i-1);
+            h.drown();
         }
     }
 
     private void heapify() {
         for (int i = data.length / 2 - 1; i >= 0; i--) {
-            drown(i, data.length);
+            stack.push(i);
+            stack.push(data.length);
+            drown();
         }
     }
 
-    private void drown(int i, int size) {
-        int left = i * 2 + 1, right = i * 2 + 2;
-        int max = i;
-        if (left < size && data[i].compareTo(data[left]) < 0)
-            max = left;
+    private void drown() {
+        while (!stack.empty()) {
+            int size = stack.pop();
+            int i = stack.pop();
+            int left = i * 2 + 1, right = i * 2 + 2;
+            int max = i;
+            if (left < size && data[i].compareTo(data[left]) < 0)
+                max = left;
 
-        if (right < size && data[max].compareTo(data[right]) < 0)
-            max = right;
+            if (right < size && data[max].compareTo(data[right]) < 0)
+                max = right;
 
-        if (max != i) {
-            swap(max, i);
-            drown(max, size);
+            if (max != i) {
+                swap(max, i);
+                stack.push(max);
+                stack.push(size);
+            }
         }
     }
 
@@ -61,7 +72,9 @@ public class Heap<T extends Comparable<T>> {
         swap(0, size - 1);
         data[size - 1] = null;
         size--;
-        drown(0, size);
+        stack.push(0);
+        stack.push(size);
+        drown();
         return res;
     }
 
