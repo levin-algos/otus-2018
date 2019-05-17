5bb9561a54e172095d6b349984e356cf8e381e05
Yuriy <genius922@yandex.ru>
2019-05-17 16:32:39 +0300

    hll parallel

--- hll/src/main/java/ru/otus/algo/HyperLogLog.java
+++ hll/src/main/java/ru/otus/algo/HyperLogLog.java
@@ -1,40 +1,74 @@
 package ru.otus.algo;
 
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
 public class HyperLogLog {
 
-    private static final int BUCKETS = 16;
-    private final int BUCKET_BITS = 4;
-    private final long[] registers = new long[BUCKETS];
-//
-    void add(int hash) {
-        int r = getRegister(hash);
-        int w = (hash << BUCKET_BITS);
-        final int b = Integer.numberOfLeadingZeros(w) % (32 - BUCKET_BITS);
-        registers[r] = Math.max(registers[r], b);
+    private final int m;
+    private final int BUCKET_BITS;
+    private final int bucketMask;
+    private final AtomicIntegerArray registers;
+    private final double alphaMSquared;
+
+    HyperLogLog(int bucketsBit) {
+        this.BUCKET_BITS = bucketsBit;
+        this.m = 1 << bucketsBit;
+        this.bucketMask = (1 << bucketsBit) - 1;
+        registers = new AtomicIntegerArray(m);
+        alphaMSquared = getAlphaMSquared();
     }
 
-    private int getRegister(int hash) {
-        final int intSize = 32;
-        return hash >>>(intSize - BUCKET_BITS);
+    void add(long hash) {
+        int r = getRegister(hash);
+        long w = (hash >>> BUCKET_BITS);
+        final int b = Math.min(Long.numberOfTrailingZeros(w) + 1, 64 - BUCKET_BITS);
+        boolean updated = false;
+        while (!updated) {
+            final int val = registers.get(r);
+            if (b > val)
+                updated = registers.compareAndSet(r, val, b);
+            else updated = true;
+        }
+    }
+
+    private int getRegister(long hash) {
+        return (int) (hash & bucketMask);
     }
 
     long count() {
-        double z = getHarmonicMean(registers);
-        return (long)(getAlpha()*BUCKETS*BUCKETS*z);
+        return getEstimate();
     }
 
-    private double getHarmonicMean(long[] registers) {
+    private long getEstimate() {
         double res = 0;
-        for (long r: registers) {
-            res += Math.pow(2, -r);
+        int v = 0;
+        for (int i = 0; i < registers.length(); i++) {
+            final int r = registers.get(i);
+            res += 1.0 / (1L << r);
+            if (r == 0) v++;
         }
-        return 1.0/res;
+        res = 1.0 / res;
+        long E = (long) (alphaMSquared * res);
+
+        if (E <= 5 / 2 * m) {
+            E = v != 0 ? (long) (m * Math.log(m / v)) : E;
+        } else {
+            final long l = 1L << 32;
+            if (E > (l / 30)) {
+                E = (long)(-l * Math.log(1 - (double)E / l));
+            }
+        }
+        return E;
     }
 
-    private double getAlpha() {
-        if (BUCKETS == 16) return 0.673;
-        if (BUCKETS == 32) return 0.697;
-        if (BUCKETS == 64) return 0.709;
-        return 0;
+    private double getAlphaMSquared() {
+        double alpha;
+        if (m == 16) alpha = 0.673;
+        else if (m == 32) alpha = 0.697;
+        else if (m == 64) alpha = 0.709;
+        else {
+            alpha = 0.7213 / (1 + (1.079 / m));
+        }
+        return alpha * m * m;
     }
 }
--- hll/src/test/java/ru/otus/algo/HLLTest.java
+++ hll/src/test/java/ru/otus/algo/HLLTest.java
@@ -2,31 +2,42 @@ package ru.otus.algo;
 
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
-import org.assertj.core.data.Offset;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 
 import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HLLTest {
 
-    long numberOfElements = 100_000_000;
-    long toleratedDifference = 1_000_000;
     HashFunction hashFunction = Hashing.murmur3_128();
 
-    @Test
-    void basicTest() {
-        final HyperLogLog hll = new HyperLogLog();
-        LongStream.range(0, numberOfElements).forEach(element -> {
-            int hashedValue = hashFunction.newHasher().putLong(element).hash().asInt();
-            hll.add(hashedValue);
-        }
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    void basicTest(int distinct, int error) {
+        int i = 8;
+            System.out.println(i);
+            final HyperLogLog hll = new HyperLogLog(i);
+            LongStream.range(0, distinct).parallel().forEach(element -> {
+                long hashedValue = hashFunction.newHasher().putLong(element).hash().asLong();
+                hll.add(hashedValue);
+            });
+            long count = hll.count();
+            System.out.println(String.format("real: %d, hll: %d, error: %f", distinct, count, 100 - (double) count / distinct * 100));
+//        assertThat(count).isCloseTo(numberOfElements, Offset.offset(toleratedDifference));
+    }
+
+    static Stream<? extends Arguments> dataProvider() {
+        return Stream.of(
+//                Arguments.of(100_000, 1_000),
+//                Arguments.of(1_000_000, 10_000),
+//                Arguments.of(10_000_000, 100_000),
+                Arguments.of(100_000_000, 1_000_000),
+                Arguments.of(1_000_000_000, 10_000_000)
         );
-
-        long count = hll.count();
-
-        assertThat(count).isCloseTo(numberOfElements, Offset.offset(toleratedDifference));
     }
 }
