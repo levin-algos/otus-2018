fcbf17fbaa885ac7639f7790caff99dd124623d9
Yuriy <levin-algos@yandex.ru>
2019-05-18 20:11:21 +0300

    britannica test

--- hll/src/test/java/ru/otus/algo/HLLTest.java
+++ hll/src/test/java/ru/otus/algo/HLLTest.java
@@ -2,6 +2,7 @@ package ru.otus.algo;
 
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -19,23 +20,21 @@ public class HLLTest {
     @ParameterizedTest
     @MethodSource("dataProvider")
     void basicTest(int distinct, int error) {
-        int i = 8;
-            System.out.println(i);
-            final HyperLogLog hll = new HyperLogLog(i);
-            LongStream.range(0, distinct).parallel().forEach(element -> {
-                long hashedValue = hashFunction.newHasher().putLong(element).hash().asLong();
-                hll.add(hashedValue);
-            });
-            long count = hll.count();
-            System.out.println(String.format("real: %d, hll: %d, error: %f", distinct, count, 100 - (double) count / distinct * 100));
-//        assertThat(count).isCloseTo(numberOfElements, Offset.offset(toleratedDifference));
+        final HyperLogLog hll = new HyperLogLog(14);
+        LongStream.range(0, distinct).parallel().forEach(element -> {
+            long hashedValue = hashFunction.newHasher().putLong(element).hash().asLong();
+            hll.add(hashedValue);
+        });
+
+        long count = hll.count();
+        assertThat(count).isCloseTo(distinct, Offset.offset((long)error));
     }
 
     static Stream<? extends Arguments> dataProvider() {
         return Stream.of(
-//                Arguments.of(100_000, 1_000),
-//                Arguments.of(1_000_000, 10_000),
-//                Arguments.of(10_000_000, 100_000),
+                Arguments.of(100_000, 1_000),
+                Arguments.of(1_000_000, 10_000),
+                Arguments.of(10_000_000, 100_000),
                 Arguments.of(100_000_000, 1_000_000),
                 Arguments.of(1_000_000_000, 10_000_000)
         );
